8b9b497b2eed822a282310afbf1d92a68a88c50e
jechynge <ching.jordan@gmail.com>
2014-01-25 14:24:13 -0700

    First version
    Has movement and some basic placeholders.

---
+++ Assets/movin.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movin : MonoBehaviour {
+
+	private Vector3 moveDirection = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += Vector3.right * 0.1f;
+
+
+	}
+}
